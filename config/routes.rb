224807fe7968fe9fc374b7968c6fe4d22814e4c3
Vincent Huche <vincent@huche.net>
2020-11-10 15:47:30 +0100

    Ex 2 updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get "/tasks/new", to: "tasks#new"
   post "/tasks", to: "tasks#create"
   get "/tasks/:id", to: "tasks#show", as: "task"
+  get "tasks/:id/edit", to: "tasks#edit", as: "task_edit"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: "task_destroy"
 end
